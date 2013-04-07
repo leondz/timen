96f6c7c04110014f0ca553d87d2e0e43da43afa8
hectorlm1983@gmail.com <hectorlm1983@gmail.com@41d8a66e-5842-b178-e407-f0cc2d12f54c>
2013-04-07 19:05:21 +0000

    [maven-release-plugin]  copy for tag timen-1.0.2

---
+++ tags/timen-1.0.2/src/org/timen/rules/Rule_Engine.java
@@ -0,0 +1,107 @@
+package org.timen.rules;
+
+import org.timen.*;
+import org.timen.grammar.*;
+import java.io.*;
+import org.antlr.runtime.*;
+
+/**
+ *
+ * @author hector
+ */
+public class Rule_Engine {
+
+    /**
+     * Appy a valid rule:
+     *
+     * The language has different variables and keywords to read
+     *
+     * @param rule
+     * @param timen_object
+     * @param timex_object
+     * @return String consisting of the normalized value for the timex_object
+     */
+    public static String apply(Rule rule, TIMEN timen_object, TIMEX_Instance timex_object) {
+        String result = "";
+
+        try {
+
+        ANTLRInputStream rule_input = new ANTLRInputStream(new ByteArrayInputStream((rule.get_rule()).getBytes("UTF-8")));
+        TIMENruleGrammarLexer rule_lexer = new TIMENruleGrammarLexer(rule_input);
+        CommonTokenStream rule_tokens = new CommonTokenStream(rule_lexer);
+        TIMENruleGrammarParser rule_parser = new TIMENruleGrammarParser(rule_tokens);
+
+        // TODO currently this switch is quite useless
+
+            switch (Rule.NormTaxonomy.valueOf(rule.get_rule_type())) {
+                case explicit: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+                case implicit_deictic: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+                case implicit_anaphoric: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+                case durative: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+                case set: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+                case vague: {
+                    result=rule_parser.run(timen_object, timex_object);
+                }
+                break;
+
+            }
+        } catch (Exception e) {
+            System.err.println("Errors found (Rule_Engine):\n\t" + e.getMessage() + "\n");
+            if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
+                e.printStackTrace(System.err);
+            }
+            System.exit(1);
+        }
+        return result;
+    }
+
+    /*
+    public static String parse_explicit(String rule, String[] normtextarr) {
+        String result = "";
+        try {
+            // variable or function
+            if (rule.matches("PAT\\([0-9]+\\)")) {
+                return normtextarr[Integer.parseInt(rule.substring(4, rule.length()-1))];
+            }else{
+                int splitpos=rule.indexOf('(');
+                String func=rule.substring(0, splitpos);
+                String params = rule.substring(splitpos+1, rule.length()-1);
+                System.out.println(func+" ---> "+params);
+                if(params.length()>0){
+                    //Let's use ANTRL3
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Errors found (Rule_Engine):\n\t" + e.getMessage() + "\n");
+            if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
+                e.printStackTrace(System.err);
+            }
+            System.exit(1);
+        }
+
+
+        return result;
+    }
+*/
+
+}
